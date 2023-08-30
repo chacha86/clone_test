887a5f183f8d1a66917fabc42452f0ac86c57bf2
chacha86 <taejincha0619@gmail.com>
2023-08-30 14:55:41 +0900

    출력과 변수

---
+++ src/day1/Variable.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        // 홍길동의 나이를 저장했다가 필요할 때 사용
+
+        // 값을 저장하는 상자(그릇) - 변수
+        // 변수를 만들 때는 크기와 모양 결정
+        // 변수를 만들 때는 변수의 이름이 필요
+
+        // 값 -> 숫자(int), 문자(String)
+
+        int age = 25;
+
+        System.out.println("안녕하세요 저는 25살 홍길동입니다.");
+        System.out.println("안녕하세요 저는 25살 홍길동입니다.");
+        System.out.println("안녕하세요 저는 25살 홍길동입니다.");
+        System.out.println("안녕하세요 저는 25살 홍길동입니다.");
+        System.out.println("안녕하세요 저는 25살 홍길동입니다.");
+
+
+    }
+}
